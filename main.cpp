c08712fcf985579c64c6cc29e278759615106467
680615515EiThandarBo <eithandarbo8@gmail.com>
2025-12-19 14:36:27 +0630

    first try

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 int main() {
-    
+    int size=100;
+    char* word=new char[size];
+    cout<<"Enter a word: ";
+    cin.getline(word, size);
+    int length=strlen(word);
+    bool palindrome=true;
+    for(int i=0; i<length/2; i++) {
+        if(word[i] != word[length-i-1]) {
+            palindrome = false;
+            break;
+        }
+    }
+    if(palindrome){
+        cout<<"The word is a palindrome."<<endl;
+    } else {
+        cout<<"The word is not a palindrome."<<endl;
+    }
 
     return 0;
 }
