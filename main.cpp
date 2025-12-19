ccc51fb399452a78ca66d98a45e4774175182b25
680615515EiThandarBo <eithandarbo8@gmail.com>
2025-12-19 15:50:11 +0630

    make part 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 using namespace std;
 
 int main() {
@@ -22,5 +23,22 @@ int main() {
         cout << "The word is not a palindrome." << endl;
     }
     delete[] word1;
+
+    string word2;
+    bool palindrome2=true;
+    cout<<"Part2; Enter a word: ";
+    getline(cin, word2);
+    int length2 = word2.length();
+    for(int i=0; i<= length2 / 2; i++, length2--){//checking palindrome
+        if(word2[i] != word2[length2 - 1]){
+            palindrome2 = false;
+            break;
+        }
+    }
+    if(palindrome2){
+        cout<<"The word is a palindrome."<<endl;
+    }else{
+        cout<<"The word is not a palindrome."<<endl;
+    }
     return 0;
 }
